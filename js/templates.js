eb7520ba445dee707e2953a0feefdba0ed5445e4
Questforgegamespnw <questforgegamespnw@gmail.com>
2026-03-30 06:28:23 -0700

    Update templates.js

--- js/templates.js
+++ js/templates.js
@@ -3,6 +3,10 @@ const TEMPLATES = {
   stone: {
     name: "Stone Template",
     apply: (golem, player) => {
+
+      golem.traits = golem.traits || [];
+      golem.actions = golem.actions || [];
+
       golem.ac = Math.max(golem.ac, 14 + player.pb);
       golem.damageReduction = (golem.damageReduction || 0) + player.pb;
       golem.speed -= 5;
@@ -16,6 +20,9 @@ const TEMPLATES = {
   clay: {
     name: "Clay Template",
     apply: (golem, player) => {
+
+      golem.traits = golem.traits || [];
+
       golem.traits.push(
         `Regeneration. Regains ${player.pb} HP at start of turn if above 0 HP.`,
         `Acid Vulnerability. This regeneration is suppressed after taking acid damage until end of next turn.`
@@ -26,6 +33,9 @@ const TEMPLATES = {
   wood: {
     name: "Wood Template",
     apply: (golem, player) => {
+
+      golem.traits = golem.traits || [];
+
       golem.dex = Math.max(golem.dex, 14);
 
       golem.traits.push(
@@ -38,6 +48,10 @@ const TEMPLATES = {
   metal: {
     name: "Metal Template",
     apply: (golem, player) => {
+
+      golem.traits = golem.traits || [];
+      golem.actions = golem.actions || [];
+
       golem.ac = Math.max(golem.ac, 16 + player.pb);
       golem.speed -= 5;
 
@@ -50,6 +64,9 @@ const TEMPLATES = {
   earth: {
     name: "Earth Template",
     apply: (golem, player) => {
+
+      golem.traits = golem.traits || [];
+
       golem.traits.push(
         "Earthbound. Advantage on STR checks and saves while touching ground.",
         "Terrain Mastery. Ignores difficult terrain from earth/stone.",
@@ -61,6 +78,10 @@ const TEMPLATES = {
   cloth: {
     name: "Cloth Template",
     apply: (golem, player) => {
+
+      golem.traits = golem.traits || [];
+      golem.actions = golem.actions || [];
+
       golem.traits.push(
         "Amorphous Form. Can move through 1-inch spaces.",
         "Slippery Form. Advantage vs grapple and restraint."
@@ -75,6 +96,10 @@ const TEMPLATES = {
   bone: {
     name: "Bone Template",
     apply: (golem, player) => {
+
+      golem.traits = golem.traits || [];
+      golem.actions = golem.actions || [];
+
       golem.actions.push(
         `Necrotic Strike. Deals +1d4 necrotic damage once per turn.`
       );
@@ -88,6 +113,10 @@ const TEMPLATES = {
   blood: {
     name: "Blood Template",
     apply: (golem, player) => {
+
+      golem.traits = golem.traits || [];
+      golem.actions = golem.actions || [];
+
       golem.traits.push(
         "Liquid Form. Can move through 1-inch spaces.",
         "Predatory Grip. Advantage on grapple checks."
