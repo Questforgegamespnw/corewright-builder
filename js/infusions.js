7865afbd68006cdafa1616e08aee5136976178b1
Questforgegamespnw <questforgegamespnw@gmail.com>
2026-03-29 17:25:47 -0700

    Update infusions.js
    added condensed cards.

--- js/infusions.js
+++ js/infusions.js
@@ -1,144 +1,121 @@
 const INFUSIONS = {
-  reinforcedFrame: {
+  // 3rd-Level Infusions
+  reinforced_frame: {
     name: "Reinforced Frame",
     tags: ["Tank"],
-    apply: (golem, player) => {
-      golem.hp += player.level;
-      golem.ac += 1;
-    }
+    effect: "Golem’s hit points increase by your artificer level and gains +1 AC.",
+    details: "Bonus applied when golem is created; scales automatically with level."
   },
-
-  warConstruct: {
+  war_construct: {
     name: "War Construct",
     tags: ["DPS"],
-    apply: (golem) => {
-      golem.multiAttack = true;
-      golem.bonusDamage = (golem.bonusDamage || 0) + 3.5; // avg 1d6
-    }
+    effect: "Golem can make two attacks instead of one when taking the Attack action.",
+    details: "Each attack deals an additional 1d6 damage; damage type matches attack."
   },
-
-  dexterousManipulators: {
+  dexterous_manipulators: {
     name: "Dexterous Manipulators",
     tags: ["Utility"],
-    apply: (golem) => {
-      golem.dex = Math.max(golem.dex, 14);
-      golem.weaponUser = true;
-    }
+    effect: "Golem’s Dexterity becomes 14 and gains proficiency with simple weapons.",
+    details: "Can take the Use an Object action without requiring command."
   },
-
-  reactivePlating: {
+  accelerated_servos: {
+    name: "Accelerated Servos",
+    tags: ["Utility"],
+    effect: "Golem’s movement speed increases by 10 feet.",
+    details: "Can take Dash or Disengage as a bonus action without command."
+  },
+  reactive_plating: {
     name: "Reactive Plating",
     tags: ["Tank"],
-    apply: (golem, player) => {
-      golem.damageReduction = `1d10 + ${player.pb}`;
-    }
+    effect: "Golem can reduce damage by 1d10 + proficiency bonus using its reaction.",
+    details: "Can be used after damage is rolled but before applied."
   },
-
-  overchargedCore: {
+  arcane_conduit: {
+    name: "Arcane Conduit",
+    tags: ["Utility"],
+    effect: "Golem can deliver touch-range spells you cast.",
+    details: "Golem must be within 30 feet; spell originates from golem."
+  },
+  anchored_frame: {
+    name: "Anchored Frame",
+    tags: ["Tank", "Control"],
+    effect: "Golem has advantage on saving throws vs being knocked prone, pushed, or pulled.",
+    details: "Forced movement affecting golem is reduced to 10 feet."
+  },
+  overcharged_core: {
     name: "Overcharged Core",
     tags: ["DPS"],
-    apply: (golem, player) => {
-      golem.burstDamage = 10;
-      golem.selfDamage = player.pb;
-    }
+    effect: "Once per turn, golem deals +10 force damage on hit.",
+    details: "After using, golem takes force damage equal to proficiency bonus."
   },
-
-  graviticCore: {
-    name: "Gravitic Core",
+  sentinel_protocol: {
+    name: "Sentinel Protocol",
     tags: ["Control"],
-    apply: (golem, player) => {
-      golem.controlEffect = "STR save or prone/pull";
-      golem.saveDC = player.spellDC;
-    }
+    effect: "Golem can use reaction to attack creatures attacking others within 5 feet.",
+    details: "Triggers after the attack is declared."
   },
 
-  selfRepair: {
+  // 9th-Level Infusions
+  self_repair_matrix: {
     name: "Self-Repair Matrix",
     tags: ["Tank"],
-    apply: (golem, player) => {
-      golem.regen = player.intMod + player.pb;
-    }
+    effect: "At start of its turn, golem regains HP equal to Int modifier + proficiency bonus.",
+    details: "Regeneration suppressed until end of next turn if it takes acid or fire damage."
   },
-
-  siegeEngine: {
+  siege_engine: {
     name: "Siege Engine",
     tags: ["DPS"],
-    apply: (golem) => {
-      golem.objectDamageMultiplier = 2;
-      golem.bonusDamage = (golem.bonusDamage || 0) + 4.5; // avg 1d8
-      golem.grantsAdvantage = true;
-    }
+    effect: "Golem deals double damage to objects and structures.",
+    details: "Attacks deal +1d8; attack rolls against golem have advantage until next turn."
   },
-
-  reflexiveCounter: {
+  reflexive_countermeasures: {
     name: "Reflexive Countermeasures",
     tags: ["Control"],
-    apply: (golem) => {
-      golem.reactionAttack = true;
-    }
+    effect: "Golem can make a melee attack as reaction when a creature misses it.",
+    details: "Triggers once per round."
   },
 
-  cognitiveMatrix: {
+  // 15th-Level Infusions
+  cognitive_matrix: {
     name: "Cognitive Matrix",
     tags: ["Utility"],
-    apply: (golem, player) => {
-      golem.allSaveProf = true;
-      golem.intBonus = player.intMod;
-    }
+    effect: "Golem gains proficiency in all saving throws.",
+    details: "Adds Int modifier to ability checks and saving throws; can Help as bonus action."
   },
-
-  phaseShifter: {
+  phase_shifter: {
     name: "Phase Shifter",
     tags: ["Utility"],
-    apply: (golem) => {
-      golem.phasing = true;
-    }
+    effect: "Golem becomes incorporeal until start of next turn.",
+    details: "Can move through creatures and objects as difficult terrain."
   },
-
-  overdrive: {
+  overdrive_protocol: {
     name: "Overdrive Protocol",
     tags: ["DPS"],
-    apply: (golem, player) => {
-      golem.extraAttack = true;
-      golem.selfDamage = player.pb;
-    }
+    effect: "Golem makes one additional attack when commanded to attack.",
+    details: "At end of turn, it takes force damage equal to your proficiency bonus."
   },
-
-  adaptivePlating: {
+  adaptive_plating: {
     name: "Adaptive Plating",
     tags: ["Tank"],
-    apply: (golem) => {
-      golem.adaptiveResist = true;
-    }
+    effect: "Golem can use reaction to gain resistance to damage it takes.",
+    details: "Resistance lasts until end of next turn."
   },
-
-  replicationMatrix: {
+  replication_matrix: {
     name: "Replication Matrix",
     tags: ["Utility"],
-    apply: (golem, player) => {
-      golem.cloneHP = player.level;
-    }
+    effect: "Golem can create a duplicate in an adjacent space as an action.",
+    details: "Duplicate has hit points equal to artificer level and lasts 1 minute."
   },
-
-  elementalConvergence: {
+  elemental_convergence: {
     name: "Elemental Convergence",
     tags: ["DPS"],
-    apply: (golem, player) => {
-      golem.elementalBonus = player.intMod;
-    }
+    effect: "Golem deals additional damage equal to your Int modifier when using Engine Core type.",
+    details: "If damaging multiple creatures, gain temporary hit points equal to proficiency bonus."
   },
-
-  giantFrame: {
+  giant_frame: {
     name: "Giant Frame",
     tags: ["Tank", "DPS"],
-    apply: (golem, player) => {
-      golem.sizeUp = true;
-      golem.hp += player.level * 2;
-      golem.str += 2;
-      golem.con += 2;
-      golem.reach += 5;
-      golem.speed -= 10;
-      golem.dexSaveDisadvantage = true;
-    }
+    effect: "Golem increases size by one category (max Huge).",
+    details: "Gains HP, Strength, Con, reach; speed reduced and DEX saves at disadvantage."
   }
 };
